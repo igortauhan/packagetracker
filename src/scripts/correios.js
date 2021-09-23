c85b43a39fcbdde93e93e66213399046b057e995
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-09-23 12:47:07 -0300

    Remove unnecessary async function

--- src/scripts/correios.js
+++ src/scripts/correios.js
@@ -1,7 +1,7 @@
 const axios = require('axios').default;
 
-module.exports = async (code) => {
-    await axios.get(`https://rastreamento.correios.com.br/app/resultado.php?objeto=${code}&mqs=S`)
+module.exports = (code) => {
+    axios.get(`https://rastreamento.correios.com.br/app/resultado.php?objeto=${code}&mqs=S`)
         .then(response => {
             console.log(response.data)
         })
