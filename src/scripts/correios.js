d55432d1b75b61550a95c9801f926b9f6bff1f07
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-09-23 22:00:59 -0300

    Temporary fix for packages not found

--- src/scripts/correios.js
+++ src/scripts/correios.js
@@ -4,6 +4,10 @@ const formatter = require('./formatter');
 module.exports = async (code, ctx) => {
     axios.get(`https://rastreamento.correios.com.br/app/resultado.php?objeto=${code}&mqs=S`)
         .then(response => {
+            if (response.data.erro == true) {
+                ctx.reply("Pacote não encontrado na base de dados dos Correios");
+                return;
+            }
             let obj = formatter(response.data);
             obj.then(objFormated => {
                 ctx.reply(objFormated);
